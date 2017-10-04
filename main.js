d3045c0d1661d669f4c7a4187c59016b6b219f5a
Alex Poliakov <alpoliakov@ukr.net>
2017-10-04 18:09:08 +0300

    Add index.html, main.js, app.css

---
+++ main.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+    el: '#app',
+    components: {
+        form_registration: {
+            template: '#reg__form'
+        },
+
+        list: {
+            template: '#user__list'
+        }
+    }
+});
