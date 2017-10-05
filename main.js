31af8e6b55ab03bb1370f9ffd5bf8e3619e47080
Alex Poliakov <alpoliakov@ukr.net>
2017-10-05 19:35:03 +0300

    Some problems with the received object

--- main.js
+++ main.js
@@ -1,21 +1,81 @@
+const EventBus = new Vue();
+
 const app = new Vue({
     el: '#app',
     data() {
         return {
-            currentView: 'home',
+            currentView: 'list',
         }
     },
+
     components: {
         home: {
             template: '#home-template'
         },
 
-        formReg: {
+        form_reg: {
             template: '#form-template',
             data() {
                 return {
-                    text: 'Registration Form'
+                    text: 'Registration Form',
+                    show: false,
+                    user: {
+                        name: '',
+                        username: '',
+                        email: '',
+                        photo: '',
+                        level: '',
+                        date: '',
+                        check: ''
+                    },
+                    listUsers: []
                 }
+            },
+            computed: {
+                havePhoto() {
+                    return this.user.photo !== '';
+                }
+            },
+            methods: {
+                clearForm() {
+                    for (let key in this.user) {
+                        this.user[key] = '';
+                    }
+                },
+                saveUser() {
+                    for (let prop in this.user) {
+                        if (this.user[prop] === '') {
+                            alert(`Empty field: ${prop}`)
+                            return;
+                        }
+                    }
+                    EventBus.$emit('addUser', this.user);
+                    this.clearForm();
+
+                },
+                setUserDate(obj) {
+                    console.dir(obj);
+                },
+                getAllUsers() {
+                    let options = {
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                    };
+                    axios.get('./user.json', options)
+                        .then(response => {
+                            this.list.push(response.data);
+                            console.log(this.list[0]);
+                        })
+                        .catch(error => {
+                            let err = new Error(error);
+                            console.log(err);
+                        });
+                    this.setUserDate(this.list);
+                },
+            },
+            created() {
+                this.getAllUsers();
             }
         },
 
@@ -23,8 +83,24 @@ const app = new Vue({
             template: '#list-template',
             data() {
                 return {
-                    text: 'List User'
+                    text: 'List User',
+                    listUsers: []
                 }
+            },
+            methods: {
+                createUser(user) {
+                    return this.listUsers.push(Object.assign({}, user));
+                }
+            },
+            computed: {
+                count() {
+                    return (this.listUsers.length);
+                }
+            },
+            mounted() {
+                EventBus.$on('addUser', (user) => {
+                    return this.createUser(user);
+                });
             }
         }
     },
